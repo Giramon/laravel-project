a397507abbe8458560339f2085beb6bb4d805a4d
giramon <vankovandrei174@gmail.com>
2025-09-29 10:31:17 +0500

    create controller and blade page + route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
+
+Route::get('/reports/create', function () {
+    return view('report.create');
+})->name('reports.create');
