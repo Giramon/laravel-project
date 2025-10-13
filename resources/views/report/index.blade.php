af57fbab7dee8e1cabde34b89cf13b3a724c504a
giramon <vankovandrei174@gmail.com>
2025-10-13 08:41:58 +0500

    update routes

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -7,10 +7,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <?php 
-        require_once('./template/header.php');
-    ?>
+    <main>
+        <a href="{{ route('reports.create') }}">создать заявление</a>
+        <div class="container-flex">
+            <div class="elem-flex">
+                @foreach ($reports as $report)
+                <p class="elem-flex__p elem-flex-date">01.10.2024</p>
+                <p class="elem-flex__p elem-flex-number">A123BC 174</p>
+                <p class="elem-flex__p elem-flex-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae itaque voluptate quaerat, cumque neque qui numquam exercitationem quod laborum nihil ipsa, minima explicabo illum facilis voluptatem doloremque nesciunt iure. Praesentium.</p>
+                <p>Статус заявления - <span>подтверждено</span></p>
+                @endforeach
+            </div>
+        </div>
+    </main>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
+Route::get('/reports', [ReportController::class,'index'])->name('reports.index');
 
 Route::get('/reports/create', function () {
     return view('report.create');
