5b7f702a518c7c32b366caaca48a744eb3bd2ee8
Студенческая <@rbiu.local>
2025-11-17 09:01:17 +0500

    5 задание полностью

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,13 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Report;
+use App\Models\Status;
 
 class ReportController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $sort = $request->input('sort');
-        $reports = Report::paginate(6);
-        return view('report.index', compact('reports'));
+        if($sort != 'asc' && $sort != 'desc') {
+            $sort = 'desc';
+        }
+
+        $status = $request->input('status');
+
+        $validate = $request->validate([
+            'status' => 'exists:statuses,id'
+        ]);
+
+        if($validate) {
+            $reports = Report::where('status_id',$status)->orderBy('created_at', $sort)->paginate(6);
+        } else {
+            $reports = Report::orderBy('created_at', $sort)->paginate(6);
+        }
+
+        $statuses = Status::all();
+
+        return view('report.index', compact('reports','statuses','sort','status'));
     }
 
     public function destroy(Report $report) {
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -12,8 +12,18 @@
             <a href="{{ route('reports.create') }}">создать заявление</a>
             <div>
                 <span>Сортировка по дате создания: </span>
-                <a href="{{ route('reports.index', ['sort' => 'desc']) }}">Сначала новые</a>
-                <a href="{{ route('reports.index', ['sort' => 'asc']) }}">Сначала старые</a>
+                <a href="{{ route('reports.index', ['sort' => 'desc', 'status' => $status]) }}">Сначала новые</a>
+                <a href="{{ route('reports.index', ['sort' => 'asc', 'status' => $status]) }}">Сначала старые</a>
+            </div>
+            <div>
+                <p>Фильтрация по статусу заявки</p>
+                <ul>
+                    @foreach($statuses as $status)
+                        <li>
+                            <a href="{{route('reports.index',['sort' => $sort, 'status'=>$status->id])}}">{{$status->name}}</a>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
             <div class="container-flex">
                 @foreach ($reports as $report)
